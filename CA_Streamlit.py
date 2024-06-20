dec67c57911a25802ef7b1b0b26f71127efb36c4
patrickdowling745 <166857969+patrickdowling745@users.noreply.github.com>
2024-06-20 09:45:54 -0500

    Update CA_Streamlit.py

--- CA_Streamlit.py
+++ CA_Streamlit.py
@@ -3,6 +3,9 @@ import pandas as pd
 from sodapy import Socrata
 from datetime import datetime
 import os
+from bs4 import BeautifulSoup as soup
+import requests
+
 
 current_year = datetime.now().year
 previous_year = current_year - 1
@@ -21,7 +24,8 @@ st.markdown("""
          """, unsafe_allow_html=True)
 
 st.markdown("""The following counties are supported: <br>
-            **Cook County, IL**""", unsafe_allow_html=True)
+            **Cook County, IL** <br>
+            **Maricopa County, AZ**""", unsafe_allow_html=True)
 
 # Create a file uploader and convert the file into a dataframe
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
@@ -72,7 +76,50 @@ if uploaded_file is not None:
             st.error(f"An error occurred while fetching data: {e}")
             return None
 
-    # Add a button to trigger the API connection and data fetching
+    #Maricopa, AZ API Connection 
+    def maricopa_AZ(parcel_ids):
+        # Generate a unique filename based on the current date and time
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        filename = f"Results_{timestamp}.csv"
+        
+        #API Token 
+        token = '7df3c7e8-4674-4f40-8f61-c34dfb7e9a90'
+        
+        #URL for API 
+        url = f'https://mcassessor.maricopa.gov/parcel/{parcel_ids}/propertyinfo'
+        
+        #Headers for API 
+        headers = {
+            'Authorization': token,
+            'User-Agent': 'Null'
+        }
+        
+        all_results = []
+        
+        for parcel in parcel_ids:
+            url = f'https://mcassessor.maricopa.gov/parcel/{parcel}/propertyinfo'
+            try:
+                # Fetch data for each parcel
+                response = requests.get(url, headers=headers)
+                response.raise_for_status()  # Raise an HTTPError for bad responses
+                parcel_data = response.json()
+                all_results.append(parcel_data)
+            except requests.exceptions.RequestException as e:
+                st.error(f"An error occurred while fetching data for parcel {parcel}: {e}")
+        
+        # Convert the list of results to a DataFrame
+        if all_results:
+            results_df = pd.DataFrame(all_results)
+            # Save the results to a CSV file
+            results_df.to_csv(filename, index=False)
+            st.success(f"Data has been fetched and saved to {filename}")
+            return filename
+        else:
+            st.error("No data found for the given APNs.")
+            return None
+
+    
+    # Cook, IL Button 
     if 'Cook' in df.iloc[:, 1].values:
         # Display Button to fetch Cook, IL Property Tax Data
         if st.button('Fetch Cook, IL Property Tax Data'):
@@ -87,6 +134,24 @@ if uploaded_file is not None:
                         file_name=generated_filename,
                         mime='text/csv'
                     )
+    
+    
+    
+    #Maricopa, AZ Button 
+    if 'Maricopa' in df.iloc[:,1].values:
+        #Display button to fetch Marciopa, AZ Property Tax Data 
+        if st.button('Fetch Maricopa, AZ Property Tax Data'):
+            generated_filename = maricopa_AZ(APN_List)
+
+            # Provide a download button for the newly created file if it exists
+            if generated_filename and os.path.exists(generated_filename):
+                with open(generated_filename, 'rb') as file:
+                    st.download_button(
+                        label="Download CSV",
+                        data=file,
+                        file_name=generated_filename,
+                        mime='text/csv'
+                    )
+
 else:
     st.info("No file uploaded.")
-
